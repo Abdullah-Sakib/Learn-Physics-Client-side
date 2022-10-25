f85f52da4c2fa02033de4a73ac8da41331bed8c9
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-10-25 14:22:03 +0600

    Router setup and navbar

---
+++ src/Routes/Router/router.js
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+import Home from "../../pages/Home/Home";
+import Login from "../../pages/Login/Login";
+import Register from "../../pages/Register/Register";
+
+export const router = createBrowserRouter([
+  {
+    paht: '/', 
+    element: <Main></Main>,
+    children: [
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      }
+    ]
+  },
+
+])
