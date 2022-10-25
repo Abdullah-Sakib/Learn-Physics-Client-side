da6573335bd963b88b638b70c8e151d3dcde6acc
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-10-25 19:45:02 +0600

    Styled the side nav

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import CoursesCart from './CoursesCart';
+import { FaAngleDoubleRight } from "react-icons/fa";
 
 const Courses = () => {
   const data = useLoaderData();
   return (
     <div className='grid grid-cols-12 '>
-      <div className='col-span-3  text-start'>
+      <div className='col-span-3 px-5 mt-5 text-start'>
         {
-          data.map(option => <p key={option.id} className='mb-3'>{option.name}</p>)
+          data.map(option => <p key={option.id} className='mb-4 font-semibold text-lg text-gray-700'> <FaAngleDoubleRight className='mr-2 inline text-orange-400 text-xl'></FaAngleDoubleRight> {option.name}</p>)
         }
       </div>
       <div className='col-span-9 px-5 grid grid-cols-3 gap-5 my-5 '>
