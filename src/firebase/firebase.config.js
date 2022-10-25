05827f8038942b0a40e46adc194ee149b95b0857
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-10-25 14:40:20 +0600

    firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9C0ktNaye7vKLWwYpIsUPOrgH37ll5qA",
+  authDomain: "learn-physics-f2810.firebaseapp.com",
+  projectId: "learn-physics-f2810",
+  storageBucket: "learn-physics-f2810.appspot.com",
+  messagingSenderId: "933310887790",
+  appId: "1:933310887790:web:2bc7925f5e12cde701337f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
