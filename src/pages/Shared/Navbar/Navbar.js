058a84246e805b1315867ba663a5ec9d9ab25ca3
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-12-21 09:43:52 +0600

    Changed buttons styles

--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -88,7 +88,7 @@ const Navbar = () => {
               ) : (
                 <Link
                   to="/login"
-                  className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                  className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none btn btn-primary"
                   aria-label="Log in"
                   title="Log in"
                 >
@@ -212,7 +212,7 @@ const Navbar = () => {
                         ) : (
                           <Link
                             to="/login"
-                            className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-black transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                            className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-primary hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                             aria-label="Log in"
                             title="Log in"
                           >
