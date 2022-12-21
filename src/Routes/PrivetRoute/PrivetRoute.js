97b06ce5e42ee18faba0c1da8757ebfe1679c354
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-12-21 09:47:11 +0600

    Added loading spinner

--- src/Routes/PrivetRoute/PrivetRoute.js
+++ src/Routes/PrivetRoute/PrivetRoute.js
@@ -7,7 +7,14 @@ const PrivetRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="min-h-[80vh] flex justify-center items-center">
+        <div
+          className="spinner-border border-dashed animate-spin inline-block w-12 h-12 border-4 rounded-full border-primary"
+          role="status"
+        ></div>
+      </div>
+    );
   }
   if (user) {
     return children;
